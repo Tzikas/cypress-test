faba04a4273a3adaad4b14dba19fbcd4b793c5d4
Tzikas <niko.tzikas@gmail.com>
2025-03-17 08:09:39 -0400

    !

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -7,7 +7,7 @@ describe('My First Test', () => {
 describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('http://localhost:3000')
-    cy.contains('Hello')
+    cy.contains('Hellosd')
   })
 })
 
